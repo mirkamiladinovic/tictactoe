2a103ad5ec95eebc904110b0252d870734aee3b6
mirkamiladinovic <91729515+mirkamiladinovic@users.noreply.github.com>
2021-10-07 15:25:45 +0200

    Update tictactoe_namalodrugacijinacin.py

--- tictactoe_namalodrugacijinacin.py
+++ tictactoe_namalodrugacijinacin.py
@@ -1,39 +1,37 @@
-gameboard = {'Gornje_l':' ', 'Gornje_s':' ', 'Gornje_d':' ',
-              'Srednje_l':' ', 'Srednje_s':' ', 'Srednje_d':' ',
-              'Donje_l':' ', 'Donje_s':' ', 'Donje_d':' '}
+"""
+gameboard = {'Gore_lijevo':' ', 'Gore_srednje':' ', 'Gore_desno':' ',
+              'Srednje_lijevo':' ', 'Srednje_srednje':' ', 'Srednje_desno':' ',
+              'Dolje_lijevo':' ', 'Dolje_srednje':' ', 'Dolje_desno':' '}
+"""
+
+gameboard = {'GL':' ', 'GS':' ', 'GD':' ',
+              'SL':' ', 'SS':' ', 'SD':' ',
+              'DL':' ', 'DS':' ', 'DD':' '}
 
 def print_gameboard(gameboard):  
-        print('  ', gameboard['Gornje_l'], '  |','  ' ,gameboard['Gornje_s'], '  |', '  ',gameboard['Gornje_d'])
+        print('  ', gameboard['GL'], '  |','  ' ,gameboard['GS'], '  |', '  ',gameboard['GD'])
         print('-------+--------+--------')
-        print('  ', gameboard['Srednje_l'], '  |','  ' ,gameboard['Srednje_s'], '  |', '  ',gameboard['Srednje_d'])
+        print('  ', gameboard['SL'], '  |','  ' ,gameboard['SS'], '  |', '  ',gameboard['SD'])
         print('-------+--------+--------')
-        print('  ', gameboard['Donje_l'], '  |','  ' ,gameboard['Donje_s'], '  |', '  ',gameboard['Donje_d'], '\n')
+        print('  ', gameboard['DL'], '  |','  ' ,gameboard['DS'], '  |', '  ',gameboard['DD'], '\n')
 
 def check_gameboard(gameboard):
-    if gameboard['Gornje_l'] == gameboard['Gornje_s'] == gameboard['Gornje_d'] != ' ' or \
-        gameboard['Gornje_l'] == gameboard['Srednje_l'] == gameboard['Donje_l'] != ' ' or \
-            gameboard['Gornje_s'] == gameboard['Srednje_s'] == gameboard['Donje_s'] != ' ' or \
-                gameboard['Gornje_d'] == gameboard['Srednje_d'] == gameboard['Donje_d'] != ' ' or \
-                    gameboard['Srednje_l'] == gameboard['Srednje_s'] == gameboard['Srednje_d'] != ' ' or \
-                        gameboard['Donje_l'] == gameboard['Donje_s'] == gameboard['Donje_d'] != ' ' or \
-                            gameboard['Gornje_l'] == gameboard['Srednje_s'] == gameboard['Donje_d'] != ' ' or \
-                                gameboard['Donje_l'] == gameboard['Srednje_s'] == gameboard['Gornje_d'] != ' ':
+    if gameboard['GL'] == gameboard['GS'] == gameboard['GD'] != ' ' or \
+        gameboard['GL'] == gameboard['SL'] == gameboard['DL'] != ' ' or \
+            gameboard['GS'] == gameboard['SS'] == gameboard['DS'] != ' ' or \
+                gameboard['GD'] == gameboard['SD'] == gameboard['DD'] != ' ' or \
+                    gameboard['SL'] == gameboard['SS'] == gameboard['SD'] != ' ' or \
+                        gameboard['DL'] == gameboard['DS'] == gameboard['DD'] != ' ' or \
+                            gameboard['GL'] == gameboard['SS'] == gameboard['DD'] != ' ' or \
+                                gameboard['DL'] == gameboard['SS'] == gameboard['GD'] != ' ':
         return True
     else: return False
 
-"""
-def restart_gameboard():
-    gameboard = {'Gornje_l':' ', 'Gornje_s':' ', 'Gornje_d':' ',
-              'Srednje_l':' ', 'Srednje_s':' ', 'Srednje_d':' ',
-              'Donje_l':' ', 'Donje_s':' ', 'Donje_d':' '}
-"""
-
 turn = 'X'
 brojac_poteza = 0
-#for i in range(9):
 game_end = False
 print_gameboard(gameboard)
-
+#for i in range 9:
 while not game_end:
     print('Na potezu je', turn ,'. Izaberite mjesto')
     move = input()
@@ -69,38 +67,3 @@ while not game_end:
         turn = 'X'
 print_gameboard(gameboard)
 
-
-"""
-def igra(gameboard):
-    node = "X"
-    for i in range(9):
-        unos()
-        print_gameboard(gameboard)
-        print('igrac ', node, 'je na potezu \n')
-        turn = input()
-        while True:
-            if gameboard[turn] == ' ':
-                gameboard[turn] = node
-                break
-            else:
-                print("pogresan unos, unesi", node, "negdje drugo \n")
-                unos()
-                print_gameboard(gameboard)
-                turn = input()
-        
-        provjera = check_gameboard(gameboard)
-        if provjera == 1:
-            print('Pobjednik je', node)
-            break
-        else:
-            print("Nerijeseno je, nema pobjednika")
-
-        if node == 'X':
-            node = 'O'
-        else:
-            node = 'X'
-
-igra(gameboard)
-print_gameboard(gameboard)
-
-"""
